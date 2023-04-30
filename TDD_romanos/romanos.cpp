cb25197ab22505f24782529cf7828f37d72f9e3f
TagFernandes <tag.fernandes@hotmail.com>
2023-04-30 19:10:57 -0400

    Desenvolvimento da função 'romanos_para_decimal'. Testes incluem apenas conversão de número (TEST_CASE algarismos unicos), não sendo implementada a verificação de se o número romano solicitado é válido

--- TDD_romanos/romanos.cpp
+++ TDD_romanos/romanos.cpp
@@ -1,5 +1,26 @@
 #include "romanos.hpp"
+#include <iostream>
+#include <unordered_map>
+#include <cstring>
+
+using namespace std;
+
 int romanos_para_decimal(char const * num_romano)
 {
-  return 0; 
+  unordered_map<char, int> simbolos = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
+    int result = 0;
+    int n = strlen(num_romano);
+
+    for(int i = 0; i < n; i++) {
+        int valor_atual = simbolos[num_romano[i]];
+
+        // Verifica se o próximo símbolo é de valor maior ou menor
+        if(i < n - 1 && simbolos[num_romano[i+1]] > valor_atual) {
+            result -= valor_atual;
+        } else {
+            result += valor_atual;
+        }
+    }
+
+    return result;
 }
